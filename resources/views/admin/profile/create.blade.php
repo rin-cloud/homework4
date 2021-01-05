55e38c39c90ea6b5d6de294d3e5f259607fe2009
Tabuse Rintaro2git config --global user.name Tabuse <taburin0916@gmail.com>
2021-01-05 14:25:15 +0000

    課題修正

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>プロフィール新規作成画面</title>
-    </head>
-    <body>
-        <h1>プロフィールページ</h1>
         @extends('layouts.profile')
 
 
@@ -25,5 +16,3 @@
         </div>
     </div>
 @endsection
-    </body>
-</html>
