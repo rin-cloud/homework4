093719e5ba1e8863a672c5d0399862ae097572ad
Tabuse Rintaro2git config --global user.name Tabuse <taburin0916@gmail.com>
2021-01-05 14:14:26 +0000

    11回課題修正

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -17,5 +17,13 @@
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('profile')
+<div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールページ</h2>
+            </div>
+        </div>
+    </div>
+@endsection
     </body>
 </html>
